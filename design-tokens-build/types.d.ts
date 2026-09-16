1592d56831e20754028859b8f8c2f8c9adbcc5fb
florklife <flora.lilly.jameson@gmail.com>
2026-09-16 14:09:30 +0200

    hello

---
+++ design-tokens-build/types.d.ts
@@ -0,0 +1,18 @@
+/* build: v1.21.1 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    brand1: never;
+    brand2: never;
+    neutral: never;
+    'color-3': never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
