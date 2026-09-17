a3ff64949dda7d514857b190f55e75e080846a6a
florklife <flora.lilly.jameson@gmail.com>
2026-09-17 10:36:30 +0200

    Updates theme

--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -4,10 +4,10 @@ import type {} from '@digdir/designsystemet-types';
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
-    brand1: never;
-    brand2: never;
+    periwinkle: never;
+    pink: never;
     neutral: never;
-    'color-3': never;
+    honey: never;
   }
   export interface SeverityColorDefinitions {
     info: never;
