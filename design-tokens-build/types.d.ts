82df5a0a9a35c56bb3d0bd4626d5270cdf6e3bfb
florklife <flora.lilly.jameson@gmail.com>
2026-09-17 10:56:17 +0200

    bytt farger

--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -4,10 +4,10 @@ import type {} from '@digdir/designsystemet-types';
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
-    periwinkle: never;
-    pink: never;
+    brand1: never;
+    brand2: never;
     neutral: never;
-    honey: never;
+    'color-3': never;
   }
   export interface SeverityColorDefinitions {
     info: never;
